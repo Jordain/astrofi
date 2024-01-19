3166fdc6a1dca8bc5e6a21906908dbbe1272cf75
Jordain <jordainjoyce@hotmail.com>
2024-01-18 20:27:02 -0500

    changed to pnpm to remove vulnerabilities

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import remarkToc from "remark-toc";
-import remarkCollapse from "remark-collapse";
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind(),]
-  ,
-  markdown: {
-    remarkPlugins: [
-      remarkToc,
-      [
-        remarkCollapse,
-        {
-          test: "Table of contents",
-        },
-      ],
-    ],
-    shikiConfig: {
-      theme: "one-dark-pro",
-      wrap: true,
-    },
-    extendDefaultPlugins: true,
-  },
-  vite: {
-    // ... other Vite configurations
-    resolve: {
-      alias: {
-        '@assets': '/src/assets',
-      },
-    },
-  },
+  integrations: [mdx(), sitemap(), tailwind()]
 });
